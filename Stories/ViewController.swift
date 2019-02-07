336a6017ace0ae5de8244c0149815cb28575072b
Vladimir Grichina <vgrichina@gmail.com>
2019-02-07 10:56:27 -0800

    Upload photo from library to IPFS

--- Stories/ViewController.swift
+++ Stories/ViewController.swift
@@ -9,21 +9,22 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKScriptMessageHandler {
+class ViewController: UIViewController, WKScriptMessageHandler, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var webView: WKWebView!
+    
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         print("message.body type: \(type(of: message.body))")
         
         let body = message.body as! NSDictionary
         let method = body["method"]! as! String
         if method == "loaded" {
-            let base64String = "Hello, World".data(using: .utf8)!.base64EncodedString()
-            webView.evaluateJavaScript("uploadBlob('myBlob', '\(base64String)')")
+            uploadBlob(data: "Hello, World".data(using: .utf8)!)
         }
         
         print("body: \(body)")
     }
     
-    var webView: WKWebView!
 
     func webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask) {
         //
@@ -36,7 +37,7 @@ class ViewController: UIViewController, WKScriptMessageHandler {
             contentController.add(self, name: "callback")
             let config = WKWebViewConfiguration()
             config.userContentController = contentController
-            webView = WKWebView(frame: self.view.bounds, configuration: config)
+            webView = WKWebView(frame: CGRect.zero, configuration: config)
             self.view.addSubview(webView)
             
             let webUrl = Bundle.main.bundleURL.appendingPathComponent("browser-add-readable-stream/")
@@ -45,6 +46,33 @@ class ViewController: UIViewController, WKScriptMessageHandler {
         } catch {
             print(error)
         }
+    }
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self;
+            myPickerController.sourceType = .photoLibrary
+            self.present(myPickerController, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // TODO: Does it make sense?
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // TODO: Use edited image?
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        uploadBlob(data: image.jpegData(compressionQuality: 0.8)!)
+        // TODO: Does it make sense?
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    func uploadBlob(data: Data) {
+        let base64String = data.base64EncodedString()
+        webView.evaluateJavaScript("uploadBlob('myBlob', '\(base64String)')")
     }
 }
 
