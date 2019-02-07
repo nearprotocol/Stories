9bf3a33adfdce4b4d9f7a69bc8330fed6fd56e8b
Vladimir Grichina <vgrichina@gmail.com>
2019-02-07 12:56:18 -0800

    Track requests and route responses to callbacks

--- Stories/ViewController.swift
+++ Stories/ViewController.swift
@@ -12,40 +12,42 @@ import WebKit
 class ViewController: UIViewController, WKScriptMessageHandler, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     var webView: WKWebView!
+
+    var requestId = 0
+    var callbacksByRequestId: [Int: (Any?, Any?) -> Void] = [:]
     
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         print("message.body type: \(type(of: message.body))")
-        
         let body = message.body as! NSDictionary
-        let method = body["method"]! as! String
-        if method == "loaded" {
-            uploadBlob(data: "Hello, World".data(using: .utf8)!)
-        }
-        
         print("body: \(body)")
-    }
-    
-
-    func webView(_ webView: WKWebView, stop urlSchemeTask: WKURLSchemeTask) {
-        //
+        let method = body["method"]! as! String
+        switch method {
+        case "loaded":
+            uploadBlob(data: "Hello, World".data(using: .utf8)!, callback: { hash in
+                print("Uploaded hello: \(hash)")
+            })
+        default:
+            let requestId = body["id"]! as! Int
+            let response = body["response"]
+            let error = body["error"]
+            let callback = callbacksByRequestId.removeValue(forKey: requestId)!
+            callback(error, response)
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        do {
-            let contentController = WKUserContentController()
-            contentController.add(self, name: "callback")
-            let config = WKWebViewConfiguration()
-            config.userContentController = contentController
-            webView = WKWebView(frame: CGRect.zero, configuration: config)
-            self.view.addSubview(webView)
-            
-            let webUrl = Bundle.main.bundleURL.appendingPathComponent("browser-add-readable-stream/")
-            webView.loadFileURL(webUrl.appendingPathComponent("index.html"),
-                                allowingReadAccessTo: webUrl)
-        } catch {
-            print(error)
-        }
+
+        let contentController = WKUserContentController()
+        contentController.add(self, name: "callback")
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+        webView = WKWebView(frame: CGRect.zero, configuration: config)
+        self.view.addSubview(webView)
+
+        let webUrl = Bundle.main.bundleURL.appendingPathComponent("browser-add-readable-stream/")
+        webView.loadFileURL(webUrl.appendingPathComponent("index.html"),
+                            allowingReadAccessTo: webUrl)
     }
     
     @IBAction func takePhoto(_ sender: Any) {
@@ -65,14 +67,22 @@ class ViewController: UIViewController, WKScriptMessageHandler, UIImagePickerCon
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         // TODO: Use edited image?
         let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        uploadBlob(data: image.jpegData(compressionQuality: 0.8)!)
+        uploadBlob(data: image.jpegData(compressionQuality: 0.8)!, callback: { hash in
+            print("Uploaded image: \(hash)")
+        })
         // TODO: Does it make sense?
         self.dismiss(animated: true, completion: nil)
     }
 
-    func uploadBlob(data: Data) {
+    func uploadBlob(data: Data, callback: @escaping (String) -> Void) {
         let base64String = data.base64EncodedString()
-        webView.evaluateJavaScript("uploadBlob('myBlob', '\(base64String)')")
+        requestId = requestId + 1
+        webView.evaluateJavaScript("uploadBlob(\(requestId), '\(base64String)')")
+        callbacksByRequestId[requestId] = { error, response in
+            let dataDict = (response as! NSArray)[0] as! NSDictionary
+            callback(dataDict["hash"]! as! String)
+        }
     }
+
 }
 
