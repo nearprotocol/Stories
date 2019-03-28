272229367932bab4074b5a237670b8f2899bf22f
Vladimir Grichina <vgrichina@gmail.com>
2019-03-28 15:30:42 -0700

    Fix build issues

--- studio-project/assembly/main.ts
+++ studio-project/assembly/main.ts
@@ -1,20 +1,20 @@
 import 'allocator/arena';
 export { memory };
 
-import { contractContext, globalStorage, near } from './near';
+import { storage, near } from './near';
 import { Item } from './model.near';
 
 // --- contract code goes below
 
 export function postItem(item: Item): void {
-  let id: u64 = globalStorage.getU64('lastId') + 1;
-  globalStorage.setU64('lastId', id);
-  globalStorage.setBytes('item:' + id.toString(), item.encode());
+  let id: u64 = storage.getU64('lastId') + 1;
+  storage.setU64('lastId', id);
+  storage.setBytes('item:' + id.toString(), item.encode());
 }
 
 export function getRecentItems(): Item[] {
   // TODO: Range query
-  return globalStorage.keys('item:').map<Item>((key: string, _1: i32, _2: String[]): Item => {
-    return Item.decode(globalStorage.getBytes(key));
+  return storage.keys('item:').map<Item>((key: string, _1: i32, _2: String[]): Item => {
+    return Item.decode(storage.getBytes(key));
   });
 }
